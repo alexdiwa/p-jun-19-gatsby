bd7c1e801059c685311a26eff1d4991685a9d06d
Alex <atdiwa@gmail.com>
2019-06-15 21:22:06 +1000

    fix event listener and font swap

--- src/components/gsap.js
+++ src/components/gsap.js
@@ -12,6 +12,13 @@ export default class MyAnimation extends React.Component {
     const rightEye = document.querySelector('#right-eye');
     const mouth = document.querySelector('#mouth');
 
+    // onload jiggle
+    const letters = [letterA, letterD]
+    const letterLoad = new TimelineMax({});
+    
+    letterLoad
+      .staggerTo(letters, 0.1, {rotation: 5, repeat: 3, yoyo: true, ease: Power2.easeInOut, transformOrigin: "50% 50%", delay: 1}, 0.125);
+
     const tlmLogo = new TimelineMax({paused: true, reversed: true});
     
     tlmLogo
@@ -50,22 +57,14 @@ export default class MyAnimation extends React.Component {
       tlmLogo.reverse().timeScale(2);
     });
     
-    // onload jiggle
-    const letters = [letterA, letterD]
-    const letterLoad = new TimelineMax({});
-    
-    window.addEventListener("load", () => {
-    
-      letterLoad
-        .staggerTo(letters, 0.1, {rotation: 5, repeat: 3, yoyo: true, ease: Power2.easeInOut, transformOrigin: "50% 50%", delay: 1}, 0.125)
-    });
+
   }
   
   render() {
     return (
       <div>
         <svg className="logo" style={{ width: `70px` }} viewBox="0 0 113 89" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="1.5">
-          <Link id="homepage" to="/">
+          <Link to="/">
           <path id="a" d="M45.452,21.193c0,0 1.247,9.783 -2.423,15.718c-3.427,5.542 -10.969,9.942 -20.496,9.66c-19.728,-0.583 -20.592,-28.891 -7.719,-37.947c10.903,-7.669 25.537,-0.074 28.681,7.289c4.342,10.168 3.618,12.506 4.144,20.509c0.466,7.081 0.079,9.256 0.238,12.434" fill="none" stroke="#000" strokeWidth="6"/>
           <path id="d" d="M103.392,45.792c0,0 -29.804,-7.678 -33.612,11.111c-3.808,18.789 7.434,28.34 22.364,25.488c14.929,-2.852 14.843,-17.955 14.222,-25.789c-2.606,-32.846 -12.432,-48.603 -13.454,-50.273" fill="none" stroke="#000" strokeWidth="6"/>
           <path id="mouth" d="M45.277,45.083c0,0 1.639,2.969 5.28,2.327c1.92,-0.339 3.296,-1.524 3.48,-4.442" fill="none" stroke="#000" strokeWidth="5"/>
